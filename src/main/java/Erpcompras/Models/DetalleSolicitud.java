506a83b7042f030359793cad0e1b80b9cb08a503
SebasLzCb <sebas2006loza@gmail.com>
2025-04-28 07:23:19 -0500

    FEAT(UPS-003): Crear clase DetalleSolicitud con atributos de cantidad y precio (Archivo: DetalleSolicitud.java)

---
+++ src/main/java/Erpcompras/Models/DetalleSolicitud.java
@@ -0,0 +1,32 @@
+package Erpcompras.Models;
+
+public class DetalleSolicitud {
+    private Producto producto;
+    private int cantidad;
+
+    public DetalleSolicitud(Producto producto, int cantidad) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public double calcularSubTotal() {
+        return producto.getPrecioUnitario() * cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Detalle{" +
+                "producto=" + producto.getNombre() +
+                ", cantidad=" + cantidad +
+                ", subtotal=" + calcularSubTotal() +
+                '}';
+    }
+}
