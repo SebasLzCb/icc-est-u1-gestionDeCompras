3694d61a9cb4b82e8f25be0defa3d7ec8d0c8ee5
SebasLzCb <sebas2006loza@gmail.com>
2025-04-28 07:23:31 -0500

    FEAT(UPS-004): Crear clase Empleado heredando de Persona (Archivo: Empleado.java) (Relación: Herencia Persona -> Empleado)

--- src/main/java/Erpcompras/Models/Empleado.java
+++ src/main/java/Erpcompras/Models/Empleado.java
@@ -3,18 +3,16 @@ package Erpcompras.Models;
 public class Empleado extends Persona {
     private Departamento departamento;
 
-    public Empleado(String id, String nombre, Departamento departamento) {
-        super(id, nombre);
+    public Empleado(String nombre, String apellido, String dni, Departamento departamento) {
+        super(nombre, apellido, dni);
         this.departamento = departamento;
     }
 
-    public Departamento getDepartamento() {
-        return departamento;
-    }
+    public Departamento getDepartamento() { return departamento; }
+    public void setDepartamento(Departamento departamento) { this.departamento = departamento; }
+
     @Override
-    public void mostrarInformacion() {
-        System.out.println("Empleado: " + nombre + ", Departamento: " + departamento.getNombre());
+    public String toString() {
+        return super.toString() + " - " + departamento.toString();
     }
-
 }
-
