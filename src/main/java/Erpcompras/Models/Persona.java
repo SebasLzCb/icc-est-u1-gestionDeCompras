88845318cff5ae064484ab6cd56bd88dab050e8c
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-01 09:16:29 -0500

    remove metodo innecesario en clase persona

--- src/main/java/Erpcompras/Models/Persona.java
+++ src/main/java/Erpcompras/Models/Persona.java
@@ -12,7 +12,8 @@ public class Persona {
     }
 
     public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre;
+    }
 
     public String getApellido() { return apellido; }
     public void setApellido(String apellido) { this.apellido = apellido; }
