6d838d22539bee019b9fbe76e0837306ee6924c1
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-04 16:33:46 -0500

    FIX(UPS-020): Implementacion de setters

--- src/main/java/Erpcompras/Models/DetalleSolicitud.java
+++ src/main/java/Erpcompras/Models/DetalleSolicitud.java
@@ -13,9 +13,15 @@ public class DetalleSolicitud {
         return producto;
     }
 
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
     public int getCantidad() {
         return cantidad;
-
+    }
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
     }
 
     public double calcularSubTotal() {
