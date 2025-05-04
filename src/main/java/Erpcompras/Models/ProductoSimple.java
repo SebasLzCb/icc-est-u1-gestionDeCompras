fc94bb64941d88228c4b9093d9b25be544ba7b8d
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-04 17:06:19 -0500

    FIX(UPS-029): Adicionar método to.String()

--- src/main/java/Erpcompras/Models/ProductoSimple.java
+++ src/main/java/Erpcompras/Models/ProductoSimple.java
@@ -1,6 +1,7 @@
 package Erpcompras.Models;
 
 public class ProductoSimple extends Producto {
+
     public ProductoSimple(int id, String nombre, double precioUnitario, UnidadMedida unidad) {
         super(id, nombre, precioUnitario, unidad);
     }
@@ -8,5 +9,11 @@ public class ProductoSimple extends Producto {
     @Override
     public double calcularSubTotal() {
         return 0;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                "\n(Tipo: ProductoSimple)";
     }
 }
