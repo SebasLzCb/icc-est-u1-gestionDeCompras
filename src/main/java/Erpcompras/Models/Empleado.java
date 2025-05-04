00290ab0419a38ce95bc11d1e98e0358763d754e
Ivanna Nievecela <ivannanievecela@gmail.com>
2025-05-04 16:35:52 -0500

    FIX(UPS-021): Edicion completa de codigo; metodos, constructores, getters y setters y toString

--- src/main/java/Erpcompras/Models/Empleado.java
+++ src/main/java/Erpcompras/Models/Empleado.java
@@ -1,19 +1,69 @@
 package Erpcompras.Models;
 
-public class Empleado extends Persona {
+public class Empleado{
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private int telefono;
     private Departamento departamento;
 
-    public Empleado(String nombre, String apellido, String dni, Departamento departamento) {
-        super(nombre, apellido, dni);
-        this.departamento = departamento;
+    public Empleado(){
 
     }
 
-    public Departamento getDepartamento() { return departamento; }
-    public void setDepartamento(Departamento departamento) { this.departamento = departamento; }
+    public Empleado(String apellido, Departamento departamento, String nombre, String dni, String telefono) {
+        this.apellido = apellido;
+        this.departamento = departamento;
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = 0;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
 
     @Override
     public String toString() {
-        return super.toString() + " - " + departamento.toString();
+        String infoDepartamento = (departamento != null) ? departamento.toString() : "Sin departamento";
+        return nombre + " " + apellido + " " + dni + " " + telefono + " " + infoDepartamento;
+
     }
 }
+
