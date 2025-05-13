8076936321b39104730e45bdb2594e87dcbe6f6b
SebasLzCb <sebas2006loza@gmail.com>
2025-05-12 19:08:47 -0500

    FIX(UPS-037): Clase base Persona usada por Proveedor.

--- src/main/java/Erpcompras/Models/Persona.java
+++ src/main/java/Erpcompras/Models/Persona.java
@@ -5,17 +5,18 @@ public class Persona {
     private String apellido;
     private String dni;
     private int telefono;
+    private String email;
 
-    public Persona(String nombre, String apellido, String dni ) {
+    public Persona(String nombre, String apellido, String dni, int telefono, String email) {
         this.nombre = nombre;
         this.apellido = apellido;
         this.dni = dni;
         this.telefono = telefono;
+        this.email = email;
     }
 
     public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre;
-    }
+    public void setNombre(String nombre) { this.nombre = nombre; }
 
     public String getApellido() { return apellido; }
     public void setApellido(String apellido) { this.apellido = apellido; }
@@ -24,13 +25,13 @@ public class Persona {
     public void setDni(String dni) { this.dni = dni; }
 
     public int getTelefono() { return telefono; }
+    public void setTelefono(int telefono) { this.telefono = telefono; }
 
-    public void setTelefono(int telefono) {
-        this.telefono = telefono;
-    }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
 
     @Override
     public String toString() {
-        return nombre + " " + apellido + " (DNI: " + dni + ")";
+        return nombre + " " + apellido + " (DNI: " + dni + ", Email: " + email + ")";
     }
 }
