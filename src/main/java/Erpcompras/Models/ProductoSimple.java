153676e52c6820a13c5c26015855b956e2ee2017
SebasLzCb <sebas2006loza@gmail.com>
2025-05-12 19:06:54 -0500

    FIX(UPS-032): Modificación de ProductoSimple para implementar cálculo de subtotal y toString personalizado.

--- src/main/java/Erpcompras/Models/ProductoSimple.java
+++ src/main/java/Erpcompras/Models/ProductoSimple.java
@@ -1,19 +1,32 @@
 package Erpcompras.Models;
 
 public class ProductoSimple extends Producto {
+    private Proveedor proveedor;
 
-    public ProductoSimple(int id, String nombre, double precioUnitario, UnidadMedida unidad) {
+    public ProductoSimple(int id, String nombre, double precioUnitario, UnidadMedida unidad, Proveedor proveedor) {
         super(id, nombre, precioUnitario, unidad);
+        this.proveedor = proveedor;
+    }
+
+    public Proveedor getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
     }
 
     @Override
     public double calcularSubTotal() {
-        return 0;
+        return getPrecioUnitario();
     }
 
     @Override
     public String toString() {
         return super.toString() +
-                "\n(Tipo: ProductoSimple)";
+                "\n(Tipo: ProductoSimple)" +
+                "\nProveedor: " + (proveedor != null
+                ? proveedor.getPersona().getNombre() + " " + proveedor.getPersona().getApellido()
+                : "Sin proveedor asignado");
     }
 }
